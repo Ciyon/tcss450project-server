41741d8145ce320f9015c73d49bc0de7dbc29489
karan-singla <ksingla1@uw.edu>
2018-04-30 11:31:10 -0700

    initial commit
    Added Node modules, Index.js & Setuptables script.

---
+++ index.js
@@ -0,0 +1,73 @@
+
+
+
+
+//express is the framework we're going to use to handle requests
+const express = require('express');
+
+//Create a new instance of express
+const app = express();
+
+const FormData = require("form-data");
+
+const bodyParser = require("body-parser");
+//This allows parsing of the body of POST requests, that are encoded in JSON
+app.use(bodyParser.json());
+//We use this create the SHA256 hash
+const crypto = require("crypto");
+
+/**
+ * Method to get a salted hash.
+ * We put this in its own method to keep consistency
+ * @param {string} pw the password to hash
+ * @param {string} salt the salt to use when hashing
+ */
+function getHash(pw, salt) {
+    return crypto.createHash("sha256").update(pw + salt).digest("hex");
+}
+
+
+function sendEmail(from, to, subject, message) {
+    let form = new FormData();
+    form.append("from", from);
+    form.append("to", to);
+    form.append("subject", subject);
+    form.append("message", message);
+    form.submit("http://cssgate.insttech.washington.edu/~cfb3/mail.php", (err, res) => {
+        if(err) console.error(err);
+        console.log(res);
+    });
+}
+
+
+
+
+
+/*
+ * Return HTML for the / end point. 
+ * This is a nice location to document your web service API
+ * Create a web page in HTML/CSS and have this end point return it. 
+ * Look up the node module 'fs' ex: require('fs');
+ */
+app.get("/", (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    for (i = 1; i < 7; i++) {
+        //write a response to the client
+        res.write('<h' + i + ' style="color:blue">Hello World!</h' + i + '>'); 
+    }
+    res.end(); //end the response
+});
+
+/* 
+* Heroku will assign a port you can use via the 'PORT' environment variable
+* To accesss an environment variable, use process.env.<ENV>
+* If there isn't an environment variable, process.env.PORT will be null (or undefined)
+* If a value is 'falsy', i.e. null or undefined, javascript will evaluate the rest of the 'or'
+* In this case, we assign the port to be 5000 if the PORT variable isn't set
+* You can consider 'let port = process.env.PORT || 5000' to be equivalent to:
+* let port; = process.env.PORT;
+* if(port == null) {port = 5000} 
+*/ 
+app.listen(process.env.PORT || 5005, () => {
+    console.log("Server up and running on port: " + (process.env.PORT || 5000));
+});
