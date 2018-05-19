19982d72c7d7e80804b7bc98be57268987a84873
Brandon G <Bgaetaniello@gmail.com>
2018-05-18 18:56:36 -0700

    adding chatid output

--- routes/chat.js
+++ routes/chat.js
@@ -21,7 +21,7 @@ router.post("/createChat", (req, res) => {
                   WHERE Username=$1`
     
     db.none(insert, [username])
-        .then(() => { res.send({ success: true, message: chatId }); })
+        .then(() => { res.send({ success: true, chatid: chatId }); })
         .catch((err) => {
             res.send({
                 success: false, error: err,
